d6d386dea7a122fe3a702a4af7a1c3c03ae0cfdf
Mihai Postelnicu <mpostelnicu@gmail.com>
2018-07-20 09:29:26 +0300

    functional version with good error reporting

--- crawler.js
+++ crawler.js
@@ -1,8 +1,30 @@
 var MongoClient = require("mongodb").MongoClient;
 var url = 'mongodb://localhost:27017';
 var request = require('request');
-var rateLimit = require('function-rate-limit');
+var async = require("async");
+var hash = require('object-hash');
 
+function requestUri(uri, callback) {
+    request
+        .get({
+                url: uri,
+                headers: {'Accept': 'application/json'}
+            },
+            function (err, res, body) {
+                if (err != null || res.statusCode != 200) {
+                    var data = {};
+                    if(body==='undefined') {
+                        data.error=err;
+                    } else {
+                        data.error = JSON.parse(body).slice(0,2);
+                    }
+                    data.uri = uri;
+                    updateError(data, callback);
+                } else {
+                    insertDoc(JSON.parse(body), callback);
+                }
+            });
+}
 
 function testList(url) {
     console.log("Requesting list " + url);
@@ -15,50 +37,53 @@ function testList(url) {
             arr = Object.values(body.data);
         }
 
-
-            arr.forEach(rateLimit(1, 1000, function (contractLink) {
-                console.log("Requesting " + contractLink.uri);
-                request
-                    .get({
-                            url: contractLink.uri,
-                            headers: {'Accept': 'application/json'}
-                        },
-                        function (err, res, body) {
-                            if (err != null || res.statusCode != 200) {
-                                var data = {};
-                                data.error = JSON.parse(body);
-                                data.uri = contractLink.uri;
-                                insertDocument(data);
-                            }
-                        });
-            }));
-
-
-        if (body.next_page_url != null) {
-            rateLimit(1, 30000,
-                testList(body.next_page_url,col));
-        }
-
-    });
-}
-
-const insertDocument = function(doc) {
-
-    MongoClient.connect(url, function (err, client) {
-        console.log("Connected correctly to mongo server");
-        const db = client.db("brims-err");
-        const col = db.collection("errors");
-        // Insert some documents
-        col.insert(doc, function (err, result) {
-            console.log("Inserted 1 document into the collection");
-            client.close();
+        async.eachOfLimit(arr, 4, function (contractLink, key, callback) {
+            console.log('Processing '+key+ " " + contractLink.uri);
+            requestUri(contractLink.uri, callback);
+        }, function (err) {
+            // if any of the uri processing produced an error, err would equal that error
+                if (err) {
+                // One of the iterations produced an error.
+                // All processing will now stop.
+                console.log('Error during page processing');
+            } else {
+                console.log('All urls have been processed successfully for ' + url);
+                //move to next page
+                if (body.next_page_url != null) {
+                    testList(body.next_page_url);
+                }
+            }
         });
+
     });
 }
 
 
+const insertDoc = function (doc, callback) {
+    MongoClient.connect(url, function (err, client) {
+        const db = client.db("brims");
+        const col = db.collection("release");
+        col.insertOne(doc, function (err, result) {
+                callback();
+                client.close();
+            });
+    });
+};
 
-    var uri = "https://birms.bandung.go.id/beta/api/contracts/year/2016";
+const updateError = function (doc, callback) {
+    MongoClient.connect(url, function (err, client) {
+        const db = client.db("brims");
+        const col = db.collection("error");
+        col.updateOne({md5: hash.MD5(doc.error)}
+            , {$set: {error: doc.error}, $addToSet: {uri: doc.uri}}, {upsert: true}, function (err, result) {
+                callback();
+                client.close();
+            });
+    });
+};
 
-    testList(uri);
 
+var uri = "https://birms.bandung.go.id/beta/api/contracts/year/2017";
+//var uri = "http://127.0.0.1:8000/api/contracts/year/2016";
+
+testList(uri);
